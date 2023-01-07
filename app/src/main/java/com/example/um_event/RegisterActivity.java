f1115bf96e5117e3b2bfc734e3eafd1a290adb8c
IdzhansKhairi <muhammadidzhanskhairi@gmail.com>
2023-01-07 19:53:48 +0800

    New updated Login, Register page

--- app/src/main/java/com/example/um_event/RegisterActivity.java
+++ app/src/main/java/com/example/um_event/RegisterActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.example.um_event.LoginActivity;
@@ -15,11 +17,29 @@ public class RegisterActivity extends AppCompatActivity {
 
     private ImageButton buttonBackLogin;
     private Button newCreatingAccountButton;
+    private EditText matricNumber, username, password, conPassword;
+    private CheckBox sportsCheck, carnivalCheck, talksCheck, artsCheck, webminarCheck, showcaseCheck, educationalCheck;
+    private String checks = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Fetch information from the registration page.
+        matricNumber = (EditText) findViewById(R.id.registerMatricNumber);
+        username = (EditText) findViewById(R.id.registerUsername);
+        password = (EditText) findViewById(R.id.registerPassword);
+        conPassword = (EditText) findViewById(R.id.registerConfirmPassword);
+
+        // Fetch information from the checkboxes
+        sportsCheck = (CheckBox) findViewById(R.id.sportCheckbox);
+        carnivalCheck = (CheckBox) findViewById(R.id.carnivalCheckbox);
+        talksCheck = (CheckBox) findViewById(R.id.talkCheckbox);
+        artsCheck = (CheckBox) findViewById(R.id.artsCheckbox);
+        webminarCheck = (CheckBox) findViewById(R.id.webinarCheckbox);
+        showcaseCheck = (CheckBox) findViewById(R.id.showcaseCheckbox);
+        educationalCheck = (CheckBox) findViewById(R.id.educationalCheckbox);
 
         // Back Button to the Login Page
         buttonBackLogin = (ImageButton) findViewById(R.id.backRegisterImageButton);
@@ -38,6 +58,26 @@ public class RegisterActivity extends AppCompatActivity {
                 openHomePage();
             }
         });
+    }
+
+    // Methods to check what is being checked
+    public void Check(View v) {
+
+        if(sportsCheck.isChecked())
+            checks = checks + "Sports ";
+        if(carnivalCheck.isChecked())
+            checks = checks + "Carnival ";
+        if(talksCheck.isChecked())
+            checks = checks + "Talks ";
+        if(artsCheck.isChecked())
+            checks = checks + "Arts ";
+        if(webminarCheck.isChecked())
+            checks = checks + "Webinar";
+        if(showcaseCheck.isChecked())
+            checks = checks + "Showcase ";
+        if(educationalCheck.isChecked())
+            checks = checks + "Educational ";
+
     }
 
     // Methods to go back to login page
