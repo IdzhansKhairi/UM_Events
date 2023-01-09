40919e5395b722c74e9d4f86ee89c4270f2881d5
Aniq <s2132561@siswa.um.edu.my>
2023-01-10 00:59:25 +0800

    calendar

---
+++ app/src/main/java/com/example/um_event/CalendarEventEditFragment.java
@@ -0,0 +1,59 @@
+package com.example.um_event;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import java.time.LocalTime;
+
+public class CalendarEventEditFragment extends Fragment
+{
+    private EditText eventNameET;
+    private TextView eventDateTV, eventTimeTV;
+
+    private LocalTime time;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_calendar_event_edit, container, false);
+        initWidgets(view);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            time = LocalTime.now();
+        }
+        eventDateTV.setText("Date: " + CalendarUtils.formattedDate(CalendarUtils.selectedDate));
+
+        Button saveEventButton = view.findViewById(R.id.save_event_button);
+        saveEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveEventAction(view);
+            }
+        });
+
+        return view;
+    }
+
+    private void initWidgets(View view) {
+        eventNameET = view.findViewById(R.id.eventNameET);
+        eventDateTV = view.findViewById(R.id.eventDateTV);
+    }
+
+    public void saveEventAction(View view) {
+        String eventName = eventNameET.getText().toString();
+        CalendarEvents newEvent = new CalendarEvents(eventName, CalendarUtils.selectedDate, time);
+        CalendarEvents.eventsList.add(newEvent);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        if (fragmentManager != null) {
+            fragmentManager.popBackStack();
+        }
+    }
+}
